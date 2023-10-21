f5128c9aa74c7f689084c2d330aeaa25b55df833
gelentolga <gelentolga@gmail.com>
2023-10-21 16:33:21 +1100

    all

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true, // Add this line to enable the app directory
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
